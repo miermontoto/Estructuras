ad73c4da748feed16af29fba871cbb7da5b9f4bf
GijonDev <underscoreBis@gmail.com>
2021-11-11 15:56:30 +0100

    listaAlumnosConMedia

--- ejercicios/ejercicioDiccionarios.java
+++ ejercicios/ejercicioDiccionarios.java
@@ -9,3 +9,20 @@ public static List<String> listaAlumnosConNota(Map<String, Double> m, double f) 
   }
   return res;
 }
+
+// Modificar el algoritmo para que guarde en una lista los alumnos con una
+// media dada.
+
+public static List<String> listaAlumnosConMedia(Map<String, List<Double>> m, double f) {
+  List<String> res = new ArrayList<>();
+  for(Map.Entry<String, List<Double>> par : m.entrySet()) {
+    if(media(par.getValue()).equals(f)) res.add(par.getKey());
+  }
+  return res;
+}
+
+public static double media(List<Double> l) {
+  double res = 0.0;
+  for(double d : l) res += l;
+  return res / l.size();
+}
